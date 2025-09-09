df0cba762c8d91c2cf69ec5ca26ad1322f3912f5
scy7he <pukar.pokhrel@gmail.com>
2025-09-09 15:07:50 +0545

    Updating appwrite code

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,56 +1,64 @@
-import { Client, Databases, ID, Query } from 'appwrite'
+import { Client, TablesDB, ID, Query } from "appwrite"
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
-const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
 
 const client = new Client()
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
 
-  const database = new Databases(client)
+const tables = new TablesDB(client)
 
 export const updateSearchCount = async (searchTerm, movie) => {
   // 1. Use Appwrite SDK to check if the search term exists in the database
   try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.equal('searchTerm', searchTerm),
-    ])
+    const result = await tables.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.equal("searchTerm", searchTerm)]
+    });
 
     // 2. If it does, update the count
-    if (result.documents.length > 0) {
-      const doc = result.documents[0];
+    if (result.rows.length > 0) {
+      const row = result.rows[0];
 
-      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
-        count: doc.count + 1,
-      })
+      await tables.updateRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: row.$id,
+        data: { count: row.count + 1 },
+      });
     // 3. If it doesn't, create a new document with the search term and count as 1
     } else {
-      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-        searchTerm,
-        count: 1,
-        movie_id: movie.id,
-        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      await tables.createRow({
+        databaseId: DATABASE_ID,
+        tableId: TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          searchTerm,
+          count: 1,
+          movie_id: movie.id,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        },
       })
     }
 
   } catch (error) {
     console.error(error);
   }
-
-
-
 }
 
 
 export const getTrendingMovies = async () => {
   try {
-    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.limit(5),
-      Query.orderDesc("count")
-    ])
+    const result = await tables.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.orderDesc("count"), Query.limit(5)],
+    });
 
-    return result.documents;
+    return result.rows;
 
   } catch (error) {
     console.error(error);
