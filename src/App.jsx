c6433546f3d29c2cbaa07aea679f29ab1898b88a
scy7he <pukar.pokhrel@gmail.com>
2025-09-11 16:12:29 +0545

    More fixes

--- src/App.jsx
+++ src/App.jsx
@@ -98,39 +98,39 @@ const App = () => {
 
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
-        </div>
 
-        {trendingMovies.length > 0 && (
-          <section className='trending'>
-            <h2>Trending Movies</h2>
+          {trendingMovies.length > 0 && (
+            <section className='trending'>
+              <h2>Trending Movies</h2>
 
-            <ul>
-              {trendingMovies.map((movie, index) => (
-                <li key={movie.$id}>
-                  <p>{index + 1}</p>
-                  <img src={movie.poster_url} alt={movie.title} />
-                </li>
-              ))}
-            </ul>
+              <ul>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
+
+          <section className='all-movies'>
+            <h2>All Movies</h2>
+
+            {isLoading
+              ? <Spinner />
+              : errorMessage
+                ? <p className='text-red-500'>{errorMessage}</p>
+                : (
+                  <ul>
+                    {movieList.map((movie) => (
+                      <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                  </ul>
+                )
+            }
           </section>
-        )}
-
-        <section className='all-movies'>
-          <h2>All Movies</h2>
-
-          {isLoading
-            ? <Spinner />
-            : errorMessage
-              ? <p className='text-red-500'>{errorMessage}</p>
-              : (
-                <ul>
-                  {movieList.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                  ))}
-                </ul>
-              )
-          }
-        </section>
+        </div>
       </div>
     </main>
   );
